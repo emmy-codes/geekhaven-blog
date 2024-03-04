4ff1b8513dc0aae50b1717fe893f43b1190db294
Emma <alunia.moon@gmail.com>
2024-03-04 10:31:25 +0100

    rename blog folder and apps.py to remove underscore
    This hopefully fixes the underscore in the admin panel

--- geekhaven blog/apps.py
+++ geekhaven blog/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class GeekhavenBlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'geekhaven_blog'
+    name = 'geekhaven blog'
