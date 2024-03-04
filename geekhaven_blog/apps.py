a29089def44797fe82336f9e2497fb10837ed8f7
Emma <alunia.moon@gmail.com>
2024-03-04 11:02:16 +0100

    add header_image to models.py and give geekhaven a verbose name on apps.py

--- geekhaven_blog/apps.py
+++ geekhaven_blog/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class GeekhavenBlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'geekhaven blog'
+    name = 'geekhaven_blog'
+    verbose_name = 'GeekHaven Blog'  # custom title shown on the admin panel
--- geekhaven_blog/models.py
+++ geekhaven_blog/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 PUBLISHED_STATUS = ((0, 'Draft'), (1, 'Published'))
 
 class BlogPost(models.Model):
     blog_header = models.CharField(max_length=250, unique=True)
+    header_image = CloudinaryField('image', default='placeholder')
     slug = models.SlugField(max_length=250, unique=True)
     blog_published_status = models.IntegerField(choices=PUBLISHED_STATUS, default=0)
     blog_content = models.TextField
     creation_date = models.DateTimeField(auto_now_add=True)
-    blog_author = models.ForeignKey(
+    blog_author = models.ForeignKey( # stores ref to the User that is the author of the post.
         User, on_delete=models.CASCADE, related_name='blog_posts'
     )
     blog_likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
