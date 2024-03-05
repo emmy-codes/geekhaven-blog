b7b72a80b81bdbca7bd3a32d8bb96f16f62a3aba
Emma <alunia.moon@gmail.com>
2024-03-05 15:23:35 +0100

    add planned models register under admin.py

--- geekhaven_blog/admin.py
+++ geekhaven_blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import BlogPost
+from .models import BlogPost  # , BlogComments, AboutGeekHaven, SearchBar
 
 admin.site.register(BlogPost)
+# admin.site.register(BlogComments)
+# admin.site.register(AboutGeekHaven)
+# admin.site.register(SearchBar)
