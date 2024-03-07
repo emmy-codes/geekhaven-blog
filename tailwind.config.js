fbb33e09b99707f558650ac7e32fa5e05b3a6423
Emma <alunia.moon@gmail.com>
2024-03-07 10:16:02 +0100

    add custom colour palette to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,34 @@
 module.exports = {
   content: ['./templates/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'purple-heart': {
+          '50': '#fef6f6',
+          '100': '#fde8ed',
+          '200': '#fbd0e1',
+          '300': '#f8afd7',
+          '400': '#f47cd2',
+          '500': '#ef48dc',
+          '600': '#df27ec',
+          '700': '#a817d9',
+          '800': '#7309b9',
+          '900': '#4b1094',
+          '950': '#200571',
+        },
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    /**
+     * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
+     * for forms. If you don't like it or have own styling for forms,
+     * comment the line below to disable '@tailwindcss/forms'.
+     */
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
 
