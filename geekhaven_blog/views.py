d910b4d6af5bfc9169afa39eba83cd686415c15a
Emma <alunia.moon@gmail.com>
2024-03-12 20:43:21 +0100

    bugfix 3 - Template doesn't exist
    Progress! Rename template_name on views.py BlogGrid class to allow main content to load

--- geekhaven_blog/views.py
+++ geekhaven_blog/views.py
@@ -4,4 +4,4 @@ from .models import BlogPost
 
 class BlogGrid(generic.ListView):
     queryset = BlogPost.objects.all().order_by('-creation_date')
-    template_name = 'geekhaven/templates/index.html'
+    template_name = 'index.html'
