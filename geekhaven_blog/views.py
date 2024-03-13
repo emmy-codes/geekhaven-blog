0876f6c8a2b0097737280a669fa75ea97a28c593
Emma <alunia.moon@gmail.com>
2024-03-13 22:43:07 +0100

    Update views.py with correct template_name
    After moving my BlogGrid content out of index.html I neglected to change the template name, so now this is correctly rendering! Hurrah!

--- geekhaven_blog/views.py
+++ geekhaven_blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import generic
 from .models import BlogPost
 
+
 class BlogGrid(generic.ListView):
     queryset = BlogPost.objects.all().order_by('-creation_date')
-    template_name = 'index.html'
+    template_name = 'blogpost_list.html'
+    context_object_name = 'blogpost_list'
