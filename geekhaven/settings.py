407175a037268bbf573e5b5c449954db59f1ebc3
Emma <alunia.moon@gmail.com>
2024-03-13 20:51:44 +0100

    add whitenoise to middleware to try to solve static file deployment issue

--- geekhaven/settings.py
+++ geekhaven/settings.py
@@ -60,6 +60,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
