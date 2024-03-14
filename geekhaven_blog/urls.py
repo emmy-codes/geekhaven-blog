c9fa4ff609f57cf5d7e24389a72c0475b13ff323
Emma <alunia.moon@gmail.com>
2024-03-14 12:52:29 +0100

    Connect blog_post.html to index.html
    Connect blog_post to index.html (now understand better how to render the content) and set up path. Get rid of base.html plan and add links to both image and Read More button (may change to whole div later)

--- geekhaven_blog/urls.py
+++ geekhaven_blog/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.BlogGrid.as_view(), name='homepage')
+    path('', views.BlogGrid.as_view(), name='homepage'),
+    path('<slug:slug>/', views.view_blog_post, name='view_blog_post')
 ]
--- geekhaven_blog/views.py
+++ geekhaven_blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import BlogPost
 
@@ -7,3 +7,11 @@ class BlogGrid(generic.ListView):
     queryset = BlogPost.objects.all().order_by('-creation_date')
     template_name = 'blogpost_list.html'
     context_object_name = 'blogpost_list'
+
+
+def view_blog_post(request, slug):
+    queryset = BlogPost.objects.filter(blog_published_status=1)
+    blog_post = get_object_or_404(queryset, url_slug=slug)
+    paginate_by = 9
+    return render(request, 'blog_post.html',
+                  {'blog_post': blog_post})
