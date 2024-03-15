80661af0ba80476b411573b2032294777d610fdd
Emma <alunia.moon@gmail.com>
2024-03-15 10:16:24 +0100

    Blog Post foundations in place
    New view for viewing blog post, rendering the unique content of the slug url from the anchor that was clicked on the index.html page, html template and basic styling in place

--- geekhaven_blog/views.py
+++ geekhaven_blog/views.py
@@ -12,5 +12,9 @@ class BlogGrid(generic.ListView):
 def view_blog_post(request, slug):
     queryset = BlogPost.objects.filter(blog_published_status=1)
     blog_post = get_object_or_404(queryset, url_slug=slug)
-    paginate_by = 9
-    return render(request, "blog_post.html", {"blog_post": blog_post})
+    
+    return render(
+        request,
+        "blog_post.html",
+        {"blog_post": blog_post}
+    )
