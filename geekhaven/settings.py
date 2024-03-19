0b638c0076bfd6fd70d4b2a364f6308ed6a07cd0
Emma <alunia.moon@gmail.com>
2024-03-19 15:59:37 +0100

    removing debug True

--- geekhaven/settings.py
+++ geekhaven/settings.py
@@ -30,7 +30,7 @@ CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
 CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
