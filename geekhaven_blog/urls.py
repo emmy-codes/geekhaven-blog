1fe0e3a78baba37b3f7794433d0ff1f4b2b69a33
Emma <alunia.moon@gmail.com>
2024-03-19 15:42:03 +0100

    Reorganise urlpatterns so register is picked up before blog post views

--- geekhaven_blog/urls.py
+++ geekhaven_blog/urls.py
@@ -2,9 +2,9 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path("", views.BlogGrid.as_view(), name="homepage"),
-    path("accounts/", include("allauth.urls")),
-    path("<slug:slug>/", views.view_blog_post, name="view_blog_post"),
     path("register/", views.register_account, name="register"),
-    path("login/", views.login, name="login")
+    path("accounts/", include("allauth.urls")),
+    path("", views.BlogGrid.as_view(), name="homepage"),
+    path("<slug:slug>/", views.view_blog_post, name="view_blog_post"),
+    path("login/", views.login, name="login"),
 ]
