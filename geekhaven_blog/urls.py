0fc7bcc3262fe8c6517cffdb4d8d1b5d7bb11dd0
Emma <alunia.moon@gmail.com>
2024-03-21 11:17:50 +0100

    create and connect login.html & urls

--- geekhaven_blog/urls.py
+++ geekhaven_blog/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path("register/", views.register_account, name="register"),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout"),
     path("accounts/", include("allauth.urls")),
     path("", views.BlogGrid.as_view(), name="homepage"),
     path("<slug:slug>/", views.view_blog_post, name="view_blog_post"),
-    path("login/", views.login, name="login"),
 ]
