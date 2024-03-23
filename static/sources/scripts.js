00080b6b64a2ab9b5c5d08a7d8b4c34eb311b22a
Emma <alunia.moon@gmail.com>
2024-03-23 18:20:53 +0100

    update upload_preset to match Cloudinary credentials

--- static/sources/scripts.js
+++ static/sources/scripts.js
@@ -3,14 +3,14 @@
 // adds Cloudinary widget
 
 function launchCloudinaryWidget() {
-const uploadWidget = cloudinary.openUploadWidget(
-    {
-        cloud_name: 'demo',
-        upload_preset: 'a5vxnzbp'
-    },
-    (error, result) => {
-    console.log(error, result);
-});
+    const uploadWidget = cloudinary.openUploadWidget(
+        {
+            cloud_name: 'demo',
+            upload_preset: 'xbunpobt'
+        },
+        (error, result) => {
+            console.log(error, result);
+        });
 
     document.getElementById("cloudinary-upload").addEventListener("click");
     uploadWidget.open();
