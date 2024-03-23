3024e3032d56f8fbbd5773650d32dc74fa4da8d0
Emma <alunia.moon@gmail.com>
2024-03-23 16:58:36 +0100

    update cloudinarywidget fn to open on button click in cosplay_submissions

--- static/sources/scripts.js
+++ static/sources/scripts.js
@@ -1,11 +1,17 @@
 
 
 // adds Cloudinary widget
+
+function launchCloudinaryWidget() {
 const uploadWidget = cloudinary.openUploadWidget(
     {
         cloud_name: 'demo',
         upload_preset: 'a5vxnzbp'
     },
     (error, result) => {
-    console.log(error, result)
+    console.log(error, result);
 });
+
+    document.getElementById("cloudinary-upload").addEventListener("click");
+    uploadWidget.open();
+};
