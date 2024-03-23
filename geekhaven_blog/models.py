9d89312532d5ddd7e5aebc1b555e972c77423548
Emma <alunia.moon@gmail.com>
2024-03-23 13:22:49 +0100

    new model for cosplay submissions
    Create class for cosplay submissions and migrate

--- geekhaven_blog/models.py
+++ geekhaven_blog/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models import Q
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.shortcuts import render
@@ -56,3 +55,17 @@ class BlogComment(models.Model):
 class UserAccount(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField()
+    
+# unique model - cosplay submissions for users to utilise CRUD on the UI
+
+class CosplaySubmission(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user can upload their own images
+    image = CloudinaryField('image')
+    # who the user is cosplaying as
+    character = models.CharField(max_length=150)
+    submission_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    
+    def __str__(self):
+        return f"{self.author.username} cosplaying as {self.character}"
