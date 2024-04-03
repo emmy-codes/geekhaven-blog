2c904e1efb6c4c94bc80ca19ddf1228a93becb8d
Emma <alunia.moon@gmail.com>
2024-04-03 19:50:32 +0200

    Fix consistency issue on naming conventions on cosplay submissions/update submissions
    I was using approval_state and submission_state(0) interchangably and it was causing errors, now everything is referring to approval_state

--- geekhaven_blog/views.py
+++ geekhaven_blog/views.py
@@ -103,22 +103,28 @@ def cosplay_submissions(request):
             # create form submission without submitting
             cosplay_submission = cosplay_submission_form.save(commit=False)
             cosplay_submission.author = request.user
-            cosplay_submission.submission_status = 0
+            cosplay_submission.approval_state = False
             cosplay_submission.save()
             messages.success(
-                request, "Your submission has been sent to an admin and is pending approval" 
+                request,
+                "Your submission has been sent to an admin and is pending approval",
             )
             return redirect("cosplay_hall_of_fame")
-    else:  
+    else:
         cosplay_submission_form = CosplaySubmissionForm()
-        return render(request, "cosplay_submissions.html", {"cosplay_submission_form": cosplay_submission_form})
-
+        return render(
+            request,
+            "cosplay_submissions.html",
+            {"cosplay_submission_form": cosplay_submission_form},
+        )
 
 
 @csrf_protect
 def cosplay_hall_of_fame(request):
     # showing all published submissions publicly
-    published_submissions = CosplaySubmission.objects.filter(approval_state=True)  # noqa
+    published_submissions = CosplaySubmission.objects.filter(
+        approval_state=True
+    )
 
     # shows pending submissions when authenticated to the specific user
     if request.user.is_authenticated:
@@ -143,21 +149,17 @@ def cosplay_hall_of_fame(request):
 # allows edit and delete on user submission
 def update_submission(request, pk):
     # ensuring request is from the post author
-    submission = get_object_or_404(
-        CosplaySubmission,
-        pk=pk,
-        author=request.user
-    )
+    submission = get_object_or_404(CosplaySubmission, pk=pk, author=request.user)
 
     if request.method == "POST":
-        form = CosplaySubmissionForm(
-            request.POST,
-            request.FILES,
-            instance=submission
-        )
+        form = CosplaySubmissionForm(request.POST, request.FILES, instance=submission)
         if form.is_valid():
             form.save()
-            messages.success(request, "Your submission has been updated")
+            submission.approval_state = False
+            submission.save(update_fields=["approval_state"])
+            messages.success(
+                request, "Your submission has been updated and is pending admin appoval"
+            )
             return redirect("cosplay_hall_of_fame")
     else:
         # create form containing the selected instance of cosplay submission
@@ -167,14 +169,18 @@ def update_submission(request, pk):
         request, "cosplay_submissions.html", {"cosplay_submission_form": form}
     )
 
+
 """ Edit the pending or published cosplay submission by sending the user to the 
 cosplay_submission form with the data from the appended submission """
+
+
 def edit_submission(request, pk):
-    
+
     submission = get_object_or_404(CosplaySubmission, pk=pk)
     form = CosplaySubmissionForm(instance=submission)
-    
+
     return render(
-        request, 'cosplay_submissions.html', {
-            'form': form, 'submission': submission, 'edit_mode': True
-            })
+        request,
+        "cosplay_submissions.html",
+        {"form": form, "submission": submission, "edit_mode": True},
+    )
